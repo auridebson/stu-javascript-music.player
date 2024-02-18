866d4279703d2e813883fe50baf714a86b6d315f
auridebson <auridebson@vutus.com>
2024-02-18 10:28:46 -0300

    Update

---
+++ script.js
@@ -0,0 +1,12 @@
+// Simulação de funções de controle do player (substitua por suas próprias funções)
+function togglePlayPause() {
+    console.log('Toggle Play/Pause');
+}
+
+function nextSong() {
+    console.log('Next Song');
+}
+
+function prevSong() {
+    console.log('Previous Song');
+}
